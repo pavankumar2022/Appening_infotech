4f6490d2017ca85f8ffa9dab97cd16dd9c2abd8b
Pavan  Kumar <100409502+pavankumar2022@users.noreply.github.com>
2022-10-13 19:05:32 +0530

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -1,6 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import userController from '../controllers/userController.js';
+import checkUserAuth from '../middleware/Authentication.js';
+//middleware
+router.use('/loggeduser', checkUserAuth)
 router.post('/signup', userController.signup);
 router.post('/login', userController.login);
+router.get('/loggeduser', userController.loggedUser);
 export {router};
